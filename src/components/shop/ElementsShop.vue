19bdf788a413798981ba979a3648ddd055928afd
Florian DAVID <florian.david@edu.univ-fcomte.fr>
2022-12-16 11:24:49 +0100

    Ajout des components Vue + modification de Shop.vue

---
+++ src/components/shop/ElementsShop.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="card">
+    <div class="card-content">
+      <a href="/shop/piece1">
+        <img src="@/assets/jpg/pieces/amc_908833k.png" style="height: 200px; width: 200px; object-fit: cover; object-position: 50% 0;">
+      </a>
+    </div>
+    <div class="card-footer">
+      piece.nom
+      <div class="alert-warning">
+        <strong>Only 18 parts in stock</strong>
+      </div>
+      35 €
+      <a href="#">
+        <button class="addToPanier">Add to cart</button>
+      </a>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "ElementsShop",
+}
+</script>
---
+++ src/components/shop/FilterShop.vue
@@ -0,0 +1,55 @@
+<template>
+    <form class="form_filter_shop" method="GET" action="/shop/filter">
+      <div class="filterElements">
+        <div>
+          <label for="type">Type de pièce :</label>
+          <select id="type" name="type">
+            <option value="">--</option>
+            <option>type.nom_piece</option> <!-- v-for -->
+          </select>
+        </div>
+
+        <br>
+        <div>
+          <label for="marque">Marque vendeuse :</label>
+          <select id="marque" name="marque">
+            <option value="">--</option>
+            <option>marque.nom_marque</option> <!-- v-for -->
+          </select>
+        </div>
+
+        <br>
+        <div>
+          <label for="price">Prix :</label>
+          <fieldset id="global">
+            <p>0</p>
+            <input type="range" id="price" name="price" multiple min="0" max="1356" step="0.01">
+            <p id="priceOut">l</p>
+          </fieldset>
+          <component :is="'script'">
+            const selectElement = document.querySelector('#price');
+            selectElement.addEventListener('change', (event) => {
+              const result = document.querySelector('#priceOut');
+              result.innerHTML = selectElement.value;
+            });
+            selectElement.addEventListener('input', (event) => {
+              const result = document.querySelector('#priceOut');
+              result.innerHTML = selectElement.value;
+            });
+          </component>
+        </div>
+      </div>
+
+      <br>
+      <input type="submit" id="bFilter" value="Filtrer">
+    </form>
+</template>
+
+<script>
+export default {
+  name: "FilterShop",
+  methods: {
+
+  }
+}
+</script>
